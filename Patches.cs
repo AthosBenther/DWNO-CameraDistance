90272c802d63f5661ba5b595aec4bc163bc14dab
Athos Benther <athum@live.com>
2023-02-28 14:25:12 -0300

    remove unecessary stuff

--- Patches.cs
+++ Patches.cs
@@ -1,16 +1,10 @@
 using HarmonyLib;
-using Il2CppInterop.Runtime;
-using System;
-using BepInEx.Logging;
-using CameraDistance;
 
 
 [HarmonyPatch(typeof(CameraScriptField))]
 [HarmonyPatch("Reset")]
 class Patches
 {
-    static IntPtr newValue = (IntPtr)0;
-        
     [HarmonyPostfix]
     static void Postfix(CameraScriptField __instance)
     {
