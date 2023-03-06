9faec6731e4d2b06de66791faa670beaa7a4b614
Athos Benther <athum@live.com>
2023-03-06 17:02:22 -0300

    Adds some more camera configs

--- Patches.cs
+++ Patches.cs
@@ -1,13 +1,3 @@
-using HarmonyLib;
-
-
-[HarmonyPatch(typeof(CameraScriptField))]
-[HarmonyPatch("Reset")]
-class Patches
-{
-    [HarmonyPostfix]
-    static void Postfix(CameraScriptField __instance)
-    {
-        __instance.distanceScaleMax = 5;
-    }
-}
+using CameraDistance;
+using HarmonyLib;[HarmonyPatch(typeof(CameraScriptField))][HarmonyPatch("Reset")]class Patches{    [HarmonyPostfix]    static void Postfix(CameraScriptField __instance)    {        __instance.wallHitRadius = Plugin.c_wallHitRadius.Value;        __instance.m_CameraRotSpeed = Plugin.c_CameraRotSpeed.Value;        __instance.minVerticalAngle = Plugin.c_minVerticalAngle.Value;        __instance.maxVerticalAngle = Plugin.c_maxVerticalAngle.Value;        __instance.distanceScaleMax = Plugin.c_distanceScaleMax.Value;        __instance.distanceScaleMin = Plugin.c_distanceScaleMin.Value;
+    }}
--- Plugin.cs
+++ Plugin.cs
@@ -1,12 +1,38 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace CameraDistance;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BasePlugin
 {
+    static public ConfigEntry<float> c_wallHitRadius;
+    static public ConfigEntry<float> c_CameraRotSpeed;
+    static public ConfigEntry<float> c_smashTime;
+    static public ConfigEntry<float> c_mouseSmashTime;
+    static public ConfigEntry<float> c_smashTimeCounter;
+    static public ConfigEntry<float> c_minVerticalAngle;
+    static public ConfigEntry<float> c_maxVerticalAngle;
+    static public ConfigEntry<float> c_distanceScaleMax;
+    static public ConfigEntry<float> c_distanceScaleMin;
+
+    private ConfigEntry<string> configGreeting;
+    private ConfigEntry<bool> configDisplayGreeting;
+
+    public Plugin()
+    {
+        c_CameraRotSpeed = Config.Bind("CameraFieldScript", "cameraRotSpeed", (float)120, "Speed the camera rotates");
+        c_minVerticalAngle = Config.Bind("CameraFieldScript", "minVerticalAngle", (float)0, "Min camera height angle");
+        c_maxVerticalAngle = Config.Bind("CameraFieldScript", "maxVerticalAngle", (float)60, "Max camera height angle");
+        c_distanceScaleMax = Config.Bind("CameraFieldScript", "distanceScaleMax", (float)5, "The farthest the camera can get to the character");
+        c_distanceScaleMin = Config.Bind("CameraFieldScript", "distanceScaleMin", (float)1, "The closest the camera can get to the character");
+    }
+
     public override void Load()
     {
         // Plugin startup logic
