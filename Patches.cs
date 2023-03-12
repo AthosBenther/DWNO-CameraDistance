beaf48e37f5763609d41b88e434b34c3929fd24b
Athos Benther <athum@live.com>
2023-03-12 01:20:02 -0300

    2.1.1 adds offset and defaultDistance

--- Patches.cs
+++ Patches.cs
@@ -1,3 +1,20 @@
 using CameraDistance;
-using HarmonyLib;[HarmonyPatch(typeof(CameraScriptField))][HarmonyPatch("Reset")]class Patches{    [HarmonyPostfix]    static void Postfix(CameraScriptField __instance)    {        __instance.wallHitRadius = Plugin.c_wallHitRadius.Value;        __instance.m_CameraRotSpeed = Plugin.c_CameraRotSpeed.Value;        __instance.minVerticalAngle = Plugin.c_minVerticalAngle.Value;        __instance.maxVerticalAngle = Plugin.c_maxVerticalAngle.Value;        __instance.distanceScaleMax = Plugin.c_distanceScaleMax.Value;        __instance.distanceScaleMin = Plugin.c_distanceScaleMin.Value;
-    }}
+using HarmonyLib;
+using UnityEngine;
+
+[HarmonyPatch(typeof(CameraScriptField))]
+[HarmonyPatch("Reset")]
+class Patches
+{
+    [HarmonyPostfix]
+    static void Postfix(CameraScriptField __instance)
+    {
+        __instance.m_CameraRotSpeed = Plugin.c_cameraRotSpeed.Value;
+        __instance.minVerticalAngle = Plugin.c_minVerticalAngle.Value;
+        __instance.maxVerticalAngle = Plugin.c_maxVerticalAngle.Value;
+        __instance.distanceScaleMax = Plugin.c_distanceScaleMax.Value;
+        __instance.distanceScaleMin = Plugin.c_distanceScaleMin.Value;
+        __instance.offset           = new Vector3(Plugin.c_offsetX.Value, Plugin.c_offsetY.Value, Plugin.c_offsetZ.Value);
+        __instance.distance         = Plugin.c_defaultDistance.Value;
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,18 +1,23 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
+using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
+using HarmonyLib.Tools;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 
 namespace CameraDistance;
 
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BasePlugin
 {
+    public static ManualLogSource Logger;
+
     static public ConfigEntry<float> c_wallHitRadius;
-    static public ConfigEntry<float> c_CameraRotSpeed;
+    static public ConfigEntry<float> c_cameraRotSpeed;
     static public ConfigEntry<float> c_smashTime;
     static public ConfigEntry<float> c_mouseSmashTime;
     static public ConfigEntry<float> c_smashTimeCounter;
@@ -20,17 +25,53 @@ public class Plugin : BasePlugin
     static public ConfigEntry<float> c_maxVerticalAngle;
     static public ConfigEntry<float> c_distanceScaleMax;
     static public ConfigEntry<float> c_distanceScaleMin;
+    static public ConfigEntry<float> c_offsetX;
+    static public ConfigEntry<float> c_offsetY;
+    static public ConfigEntry<float> c_offsetZ;
+    static private ConfigEntry<float> _c_defaultDistance;
 
     private ConfigEntry<string> configGreeting;
     private ConfigEntry<bool> configDisplayGreeting;
 
+    static public ConfigEntry<float> c_defaultDistance
+    {
+        get { return _c_defaultDistance; }
+        set
+        {
+            float input = value.Value;
+            if (input < c_distanceScaleMin.Value)
+            {
+                Logger.LogError("The configuration defaultDistance must be greater than distanceScaleMin");
+                return;
+            }
+            else if (input > c_distanceScaleMax.Value)
+            {
+                Logger.LogError("The configuration defaultDistance must be smaller than distanceScaleMax");
+                return;
+            }
+            else
+            {
+                _c_defaultDistance = value;
+            }
+        }
+    }
+
+
+
     public Plugin()
     {
-        c_CameraRotSpeed = Config.Bind("CameraFieldScript", "cameraRotSpeed", (float)120, "Speed the camera rotates");
-        c_minVerticalAngle = Config.Bind("CameraFieldScript", "minVerticalAngle", (float)0, "Min camera height angle");
-        c_maxVerticalAngle = Config.Bind("CameraFieldScript", "maxVerticalAngle", (float)60, "Max camera height angle");
-        c_distanceScaleMax = Config.Bind("CameraFieldScript", "distanceScaleMax", (float)5, "The farthest the camera can get to the character");
-        c_distanceScaleMin = Config.Bind("CameraFieldScript", "distanceScaleMin", (float)1, "The closest the camera can get to the character");
+        Logger = Log;
+        HarmonyFileLog.Enabled = true;
+
+        c_cameraRotSpeed = Config.Bind("CameraFieldScript", "cameraRotSpeed", 120f, "Speed the camera rotates");
+        c_minVerticalAngle = Config.Bind("CameraFieldScript", "minVerticalAngle", 0f, "Min camera height angle");
+        c_maxVerticalAngle = Config.Bind("CameraFieldScript", "maxVerticalAngle", 60f, "Max camera height angle");
+        c_distanceScaleMax = Config.Bind("CameraFieldScript", "distanceScaleMax", 5f, "The farthest the camera can get to the character");
+        c_distanceScaleMin = Config.Bind("CameraFieldScript", "distanceScaleMin", 1f, "The closest the camera can get to the character");
+        c_offsetX = Config.Bind("CameraFieldScript", "offsetX", 0.75f, "The X target point displacement of the camera, in relation to the character");
+        c_offsetY = Config.Bind("CameraFieldScript", "offsetY", 1.25f, "The Y target point displacement of the camera, in relation to the character");
+        c_offsetZ = Config.Bind("CameraFieldScript", "offsetZ", 0.5f, "The Z target point displacement of the camera, in relation to the character");
+        c_defaultDistance = Config.Bind("CameraFieldScript", "defaultDistance", 1f, "The default distance the camera will be every time it is loaded");
     }
 
     public override void Load()
